cd9e1979c69d7d1e77cc3f127d8a75ea4b604176
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-04 13:47:03 +0100

    Initial Commit

---
+++ tsdown.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+    {
+        exports: true,
+        minify: true,
+        tsconfig: 'tsconfig.json',
+        entry: ['src/index.ts'],
+        platform: 'node',
+        outDir: 'dist',
+        format: ['esm', 'cjs'],
+        skipNodeModulesBundle: true,
+    },
+    {
+        dts: false,
+        minify: true,
+        tsconfig: 'tsconfig.json',
+        entry: ['src/cli/index.ts'],
+        platform: 'node',
+        outDir: 'bin',
+        format: ['esm'],
+        skipNodeModulesBundle: true,
+    },
+])
